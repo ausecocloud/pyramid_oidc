1d24f98921e79179b8cb9bdc127d541be3021c43
Gerhard Weis <g.weis@griffith.edu.au>
2018-11-29 08:36:09 +1000

    add helper method to create OIDCUtility from settings derive env var name from settings key

--- src/pyramid_oidc/__init__.py
+++ src/pyramid_oidc/__init__.py
@@ -5,16 +5,15 @@ from pyramid.settings import asbool
 from .interfaces import IOIDCUtility
 from .utilities import OIDCUtility
 
-# TODO: support multiple IOIDC clients? ... lookup utilities by name
-# TODO: maybe use IOIDCProvider instead of IOIDCUtility?
 
-
-def parse_setting(settings, prefix, key, conv=None, envvar=None):
+def parse_setting(settings, prefix, key, conv=None, envvar=True):
     val = None
+    name = ''.join([prefix, key])
     if envvar:
+        envvar = name.replace('.', '_').upper()
         val = os.environ.get(envvar, None)
     if val is None:
-        val = settings.get('{}{}'.format(prefix, key), None)
+        val = settings.get(name, None)
     if val is not None and conv is not None:
         val = conv(val)
     return val
@@ -24,18 +23,18 @@ def get_session_opts(settings):
     # session opts
     prefix = 'session.'
     opts = {}
-    for key, envvar in (('factory', None), ('secret', 'SESSION_SECRET')):
+    for key, envvar in (('factory', False), ('secret', True)):
         val = parse_setting(settings, prefix, key, envvar=envvar)
         if val:
             opts[key] = val
     # get int settings
     for key in ('timeout', 'refresh'):
-        val = parse_setting(settings, prefix, key, conv=int)
+        val = parse_setting(settings, prefix, key, conv=int, envvar=False)
         if val is not None:
             opts[key] = val
     # bool vars
     for key in ('on_exception',):
-        val = parse_setting(settings, prefix, key, conv=asbool)
+        val = parse_setting(settings, prefix, key, conv=asbool, envvar=False)
         if val is not None:
             opts[key] = val
 
@@ -43,17 +42,17 @@ def get_session_opts(settings):
     prefix = 'session.cookie_opts.'
     cookie_opts = {}
     for key in ('name', 'path', 'domain'):
-        val = parse_setting(settings, prefix, key)
+        val = parse_setting(settings, prefix, key, envvar=False)
         if val:
             cookie_opts[key] = val
     # int opts
     for key in ('max_age',):
-        val = parse_setting(settings, prefix, key, conv=int)
+        val = parse_setting(settings, prefix, key, conv=int, envvar=False)
         if val is not None:
             opts[key] = val
     # bool opts
     for key in ('secure', 'httponly'):
-        val = parse_setting(settings, prefix, key, conv=asbool)
+        val = parse_setting(settings, prefix, key, conv=asbool, envvar=False)
         if val is not None:
             cookie_opts[key] = val
     if cookie_opts:
@@ -72,34 +71,31 @@ def get_session_opts(settings):
     return opts
 
 
+def build_oidc_utility(settings, prefix):
+    return OIDCUtility(
+        issuer=parse_setting(settings, 'oidc.', 'issuer'),
+        client_id=parse_setting(settings, 'oidc.', 'client_id'),
+        client_secret=parse_setting(settings, 'oidc.', 'client_secret'),
+        userid_claim=parse_setting(settings, 'oidc.', 'userid_claim'),
+        audience=parse_setting(settings, 'oidc.', 'audience'),
+        verify_aud=parse_setting(settings, 'oidc.', 'verify_aud', conv=asbool),
+    )
+
+
 def includeme(config):
     settings = config.get_settings()
 
-    utility = OIDCUtility(
-        issuer=parse_setting(settings, 'oidc.', 'issuer',
-                             envvar='OIDC_ISSUER'),
-        client_id=parse_setting(settings, 'oidc.', 'client_id',
-                                envvar='OIDC_CLIENT_ID'),
-        client_secret=parse_setting(settings, 'oidc.', 'client_secret',
-                                    envvar='OIDC_CLIENT_SECRET'),
-        userid_claim=parse_setting(settings, 'oidc.', 'userid_claim',
-                                   envvar='OIDC_USERID_CLAIM'),
-        audience=parse_setting(settings, 'oidc.', 'audience',
-                               envvar='OIDC_AUDIENCE'),
-        verify_aud=parse_setting(settings, 'oidc.', 'verify_aud',
-                                 conv=asbool, envvar='OIDC_VERIFY_AUD'),
-    )
+    utility = build_oidc_utility(settings, 'oidc.')
     config.registry.registerUtility(utility, IOIDCUtility)
 
     if asbool(settings.get('oidc.enable_views', True)):
         # TODO: could make urls configurable ...
-        #       have to add more speificity to redirect_uri in case it hase the
+        #       have to add more specificity to redirect_uri in case it hase the
         #       same route pattern as login... (e.g. require code parameter)
-        config.add_route('oidc.redirect_uri', '/redirect_uri',
+        config.add_route('pyramid_oidc.redirect_uri', '/redirect_uri',
                          request_param='code')
-        config.add_route('oidc.login', '/login')
-        config.add_route('oidc.logout', '/logout')
-        config.add_route('oidc.token', '/token')
+        config.add_route('pyramid_oidc.login', '/login')
+        config.add_route('pyramid_oidc.logout', '/logout')
         config.scan()
 
     # setup session factory if requested
@@ -108,4 +104,3 @@ def includeme(config):
     if session_factory:
         factory = config.maybe_dotted(session_factory)
         config.set_session_factory(factory(**session_opts))
-
